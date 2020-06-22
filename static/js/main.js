fef0aa06bcdf43364602d361c629510ef8a615c6
to101380 <to101380@gmail.com>
2020-06-22 11:24:43 +0800

    navbar animate

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,17 @@
 //AOS動畫
 AOS.init();
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 
 
