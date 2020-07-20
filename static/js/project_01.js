92b6195c255c8da2225884ae160e2671c63cbb4b
to101380 <to101380@gmail.com>
2020-07-21 03:25:15 +0800

    reciprocal time

--- static/js/project_01.js
+++ static/js/project_01.js
@@ -104,6 +104,55 @@ function toPoint_3(point){
 
 
 
+//時間倒數
+  /* Set the date we're counting down to */
+    var countDownDate = new Date('SEP 25, 2020 00:00:00').getTime();
+    var countDownDateTXT = '';
+    function mytime() {
+        countDownDateTxt = document.getElementById('txt').value;
+        countDownDate = new Date(countDownDateTxt).getTime();
+        document.getElementById('now-time').innerHTML = countDownDateTxt;
+    }
+
+/* Update the count down every 1 second */
+
+var x = setInterval(function() {
+    /* Get todays date and time */
+    var now = new Date().getTime();
+
+    /* Find the distance between now and the count down date */
+    var distance = countDownDate - now;
+
+    /* Time calculations for days, hours, minutes and seconds */
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    /* Display the result in the element with id="demo" */
+    document.getElementById('timer_day').innerHTML = days + 'D ';    
+    document.getElementById('timer_hr').innerHTML =  hours + 'h ';
+    document.getElementById('timer_min').innerHTML = minutes + 'm ';
+    document.getElementById('timer_sec').innerHTML = seconds + 's ';
+    
+    if(days+hours+minutes+seconds<0){      
+      $("#timer_day").text("0D");
+      $("#timer_hr").text("0h");
+      $("#timer_min").text("0m");
+      $("#timer_sec").text("0s");
+      $(".reciprocal").css("background-color","#e8e8e8");      
+     }
+
+    /* If the count down is finished, write some text */
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById('timer').innerHTML = 'EXPIRED';
+    }
+}, 1000);
+
+
+
+
 //loading page
 
 function myload() {
@@ -197,7 +246,6 @@ $("#agree").click(function(){
 			        method:"GET",
 			        url: "https://api.coinlore.net/api/ticker/?id=80",        
 			      }).done(function(msg) {   
-			        console.log(msg);
 			        var ETH_price = (msg[0].price_usd);
 			        $("#webs_price_USD").text(toPoint_3(ETH_price*Webs_price));  
 			      });
