f96566eb64f0968ed9415163d718efefa428e80e
to101380 <to101380@gmail.com>
2020-07-23 13:13:14 +0800

    assets ETH price

--- static/js/assets.js
+++ static/js/assets.js
@@ -17,12 +17,66 @@ var currentScrollPos = window.pageYOffset;
 
 
 
+var ETH_price;
+
+$(document).ready(function(){
+    $.ajax({
+        method:"GET",
+        url: "https://api.coinlore.net/api/ticker/?id=80",        
+      }).done(function(msg) {   
+        console.log(msg);
+        ETH_price = (msg[0].price_usd); 
+      
+      });
+  }) 
+
+
+
+//千分數逗點
+var thousandComma = function(number)
+
+{
+ var num = number.toString();
+ var pattern = /(-?\d+)(\d{3})/;
+  
+ while(pattern.test(num))
+ {
+  num = num.replace(pattern, "$1,$2");
+  
+ }
+ return num;
+ 
+}
+  
+
+
+
 // 換成百分比
-function toPercent(point){
+function toPercent_5(point){
+		  var str=Number(point*100).toFixed(5);
+		  str+="%";
+		  return str;
+		}
+
+function toPercent_2(point){
 		  var str=Number(point*100).toFixed(2);
 		  str+="%";
 		  return str;
 		}
+
+// 進位	
+
+
+function toPoint_2(point){
+    var str=Number(point).toFixed(2);          
+    return str;
+  }	
+
+function toPoint_3(point){
+    var str=Number(point).toFixed(3);          
+    return str;
+  }		
+
 
 
 
@@ -77,24 +131,24 @@ function toPercent(point){
 			//取得帳號餘額
 			var balance = await web3.eth.getBalance(coinbase);
 			$("#my_address").text(coinbase);
-			$("#my_balance").text(web3.utils.fromWei(balance));  //wei 轉換成 ether web3.utils.fromWei()
+			$("#ETH_VOL").text(toPoint_2(web3.utils.fromWei(balance)));  //wei 轉換成 ether web3.utils.fromWei()
 
-			var Swap_contract_address = "0x6f263Be74836fbeA71e688315a9Ff81309A0Dbc5";
-			var Swap_contract_abi = [{"constant":false,"inputs":[{"name":"_price","type":"uint256"}],"name":"reduce_price","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"swap","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_price","type":"uint256"}],"name":"raise_price","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_price","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
-			var STO_contract_address = "0xb7931a19bA57C847D92684E818Ad4E3924d86219";
-			var STO_contract_abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}]
 
-			Swap_Contract = new web3.eth.Contract(Swap_contract_abi, Swap_contract_address);
-			STO_Contract = new web3.eth.Contract(STO_contract_abi, STO_contract_address);
 
-			var STO_balance = await  STO_Contract.methods.balances(coinbase).call({});		
-			$(document).ready(function() {			           
-			        $("#STO_balance").animateNumbers(STO_balance/1000000, true, 2000 );	
-			    });
+			$(document).ready(function(){
+			    $.ajax({
+			        method:"GET",
+			        url: "https://api.coinlore.net/api/ticker/?id=80",        
+			      }).done(function(msg) {  
+			        var ETH_price = (msg[0].price_usd);
+			        $("#ETH_USD").text(toPoint_2(ETH_price*(web3.utils.fromWei(balance)))); 			      
+			      });
+			  }) 
+
 			
-			var hold_rate = await  STO_Contract.methods.balances(coinbase).call({});			
-			$("#hold_rate").text(toPercent(STO_balance/1000000000/5));
+
+			
 
 
 
@@ -105,11 +159,11 @@ function toPercent(point){
 		printPostsToConsole();
 
 
-		function invest(){
-			Swap_Contract.methods.swap().send({from: coinbase, value: "438000000000000000"}).then(function(receipt){			   
-				location.reload();
-			});
-		}
+		// function invest(){
+		// 	Swap_Contract.methods.swap().send({from: coinbase, value: "438000000000000000"}).then(function(receipt){			   
+		// 		location.reload();
+		// 	});
+		// }
 
 		
 
