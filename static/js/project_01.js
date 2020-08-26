667b4952ae6ab7c2201472f9c6eb8d5ea796b7d4
to101380 <to101380@gmail.com>
2020-08-26 16:01:17 +0800

    total_supply

--- static/js/project_01.js
+++ static/js/project_01.js
@@ -106,49 +106,49 @@ function toPoint_3(point){
 
 //時間倒數
   /* Set the date we're counting down to */
-    var countDownDate = new Date('SEP 25, 2020 00:00:00').getTime();
-    var countDownDateTXT = '';
-    function mytime() {
-        countDownDateTxt = document.getElementById('txt').value;
-        countDownDate = new Date(countDownDateTxt).getTime();
-        document.getElementById('now-time').innerHTML = countDownDateTxt;
-    }
+    // var countDownDate = new Date('SEP 25, 2020 00:00:00').getTime();
+    // var countDownDateTXT = '';
+    // function mytime() {
+    //     countDownDateTxt = document.getElementById('txt').value;
+    //     countDownDate = new Date(countDownDateTxt).getTime();
+    //     document.getElementById('now-time').innerHTML = countDownDateTxt;
+    // }
 
 /* Update the count down every 1 second */
 
-var x = setInterval(function() {
-    /* Get todays date and time */
-    var now = new Date().getTime();
+// var x = setInterval(function() {
+//     /* Get todays date and time */
+//     var now = new Date().getTime();
 
-    /* Find the distance between now and the count down date */
-    var distance = countDownDate - now;
+//     /* Find the distance between now and the count down date */
+//     var distance = countDownDate - now;
 
-    /* Time calculations for days, hours, minutes and seconds */
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     /* Time calculations for days, hours, minutes and seconds */
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    /* Display the result in the element with id="demo" */
-    document.getElementById('timer_day').innerHTML = days + 'D ';    
-    document.getElementById('timer_hr').innerHTML =  hours + 'h ';
-    document.getElementById('timer_min').innerHTML = minutes + 'm ';
-    document.getElementById('timer_sec').innerHTML = seconds + 's ';
+//     /* Display the result in the element with id="demo" */
+//     document.getElementById('timer_day').innerHTML = days + 'D ';    
+//     document.getElementById('timer_hr').innerHTML =  hours + 'h ';
+//     document.getElementById('timer_min').innerHTML = minutes + 'm ';
+//     document.getElementById('timer_sec').innerHTML = seconds + 's ';
     
-    if(days+hours+minutes+seconds<0){      
-      $("#timer_day").text("0D");
-      $("#timer_hr").text("0h");
-      $("#timer_min").text("0m");
-      $("#timer_sec").text("0s");
-      $(".reciprocal").css("background-color","#e8e8e8");      
-     }
+//     if(days+hours+minutes+seconds<0){      
+//       $("#timer_day").text("0D");
+//       $("#timer_hr").text("0h");
+//       $("#timer_min").text("0m");
+//       $("#timer_sec").text("0s");
+//       $(".reciprocal").css("background-color","#e8e8e8");      
+//      }
 
-    /* If the count down is finished, write some text */
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById('timer').innerHTML = 'EXPIRED';
-    }
-}, 1000);
+//     /* If the count down is finished, write some text */
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById('timer').innerHTML = 'EXPIRED';
+//     }
+// }, 1000);
 
 
 
@@ -234,21 +234,18 @@ $("#agree").click(function(){
 
 			var total_supply = await  Webs_ERC20.methods.totalSupply().call({});	
 			$("#total_supply").text(thousandComma(total_supply/decimal));
-
-			var out_shares = await  Webs_ERC20.methods.balances("0x45aa3752E6ae9D4c1C4C45b9e4516e1bf3aC7Ad0").call({});	
-			$("#out_shares").text(thousandComma(out_shares/decimal));
-			$("#shares_rate").text(toPercent_5(out_shares/total_supply));
+		
 
 			var webs_balance = await  Webs_ERC20.methods.balances(coinbase).call({});	
-			$("#webs_balance").text(thousandComma(webs_balance/decimal));				
-			$("#hold_rate").text(toPercent_2(webs_balance/out_shares));
+			$("#webs_balance").text(toPoint_2(webs_balance/decimal));				
+			$("#hold_rate").text(toPercent_2(webs_balance/total_supply));
 			if(webs_balance>0){
 				 $("").css("display","none"); 
 			}
 
 			
 
-			var Webs_price = await 300/(out_shares/decimal)			
+			var Webs_price = await 100/(total_supply/decimal);			
 			$("#webs_price").text(Webs_price);		
 			$(document).ready(function(){
 			    $.ajax({
@@ -256,6 +253,7 @@ $("#agree").click(function(){
 			        url: "https://api.coinlore.net/api/ticker/?id=80",        
 			      }).done(function(msg) {   
 			        var ETH_price = (msg[0].price_usd);
+			        console.log(ETH_price);
 			        $("#webs_price_USD").text(toPoint_3(ETH_price*Webs_price));  
 			      });
 			}) 
